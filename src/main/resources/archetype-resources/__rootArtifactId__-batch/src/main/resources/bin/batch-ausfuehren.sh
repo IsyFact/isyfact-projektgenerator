21868db7f6f46206980c4a5345518a0007754ec2
Gleb Dubina <gleb.dubina@materna.de>
2020-02-20 10:11:17 +0100

    RF-351: Diverse Fixes und code Aufbereitungen

--- src/main/resources/archetype-resources/__rootArtifactId__-batch/src/main/resources/bin/batch-ausfuehren.sh
+++ src/main/resources/archetype-resources/__rootArtifactId__-batch/src/main/resources/bin/batch-ausfuehren.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+set -e
 
-if [ ! "$JAVA8_HOME" ]
+if [[ ! "$JAVA8_HOME" ]]
 then
 	echo "Fehler: JAVA8_HOME ist nicht gesetzt."
 	exit 1
@@ -10,7 +11,7 @@ fi
 
 JAVA_OPTS="${JAVA_OPTS} -Duser.language=de -Duser.region=DE -Dfile.encoding=UTF-8" 
 JAVA_COMMAND=$JAVA8_HOME/bin/java
-BASEDIR=`dirname $0`/..
+BASEDIR=${0%/*}/..
 pushd $BASEDIR > /dev/null
 
 CP=classes/
@@ -24,8 +25,8 @@ do
 		fi
 done
 
-echo ${JAVA_COMMAND} -classpath "${CP}" ${JAVA_OPTS} de.bund.bva.pliscommon.batchrahmen.core.launcher.BatchLauncher $*
-${JAVA_COMMAND} -classpath "${CP}" ${JAVA_OPTS} de.bund.bva.pliscommon.batchrahmen.core.launcher.BatchLauncher $*
+echo ${JAVA_COMMAND} -classpath "${CP}" ${JAVA_OPTS} de.bund.bva.pliscommon.batchrahmen.core.launcher.BatchLauncher $@
+${JAVA_COMMAND} -classpath "${CP}" ${JAVA_OPTS} de.bund.bva.pliscommon.batchrahmen.core.launcher.BatchLauncher $@
 rueckgabe=$?
 
 popd > /dev/null
